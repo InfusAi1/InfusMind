0315ce6455167a577d9ba41db7f5ce7992613328
InfusAi1 <Global-admins@infusai.com>
2024-01-10 12:43:26 +0530

    Update app.py
    back to Flask app test

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
-# from flask import Flask
+from flask import Flask
  
-# app = Flask(__name__)
+app = Flask(__name__)
  
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
  
-# if __name__ == '__main__':
-#     app.run(debug=True,
-#             host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True,
+            host="0.0.0.0")
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+# from fastapi import FastAPI
+# from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+# app = FastAPI()
 
-html_code = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Christmas Greeting</title>
-    <style>
-        body {
-            background-color: #e74c3c;  /* Christmas red color */
-            color: #ffffff;  /* White text color */
-            font-family: 'Arial', sans-serif;
-            text-align: center;
-            padding: 50px;
-            margin: 0;
-        }
-    </style>
-</head>
-<body>
-    <h1>Hello, World! Merry Christmas 🎄</h1>
-</body>
-</html>
-"""
+# html_code = """
+# <!DOCTYPE html>
+# <html>
+# <head>
+#     <title>Christmas Greeting</title>
+#     <style>
+#         body {
+#             background-color: #e74c3c;  /* Christmas red color */
+#             color: #ffffff;  /* White text color */
+#             font-family: 'Arial', sans-serif;
+#             text-align: center;
+#             padding: 50px;
+#             margin: 0;
+#         }
+#     </style>
+# </head>
+# <body>
+#     <h1>Hello, World! Merry Christmas 🎄</h1>
+# </body>
+# </html>
+# """
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return HTMLResponse(content=html_code)
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root():
+#     return HTMLResponse(content=html_code)
 
 
-# run code locally
-import uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app,
-                host="0.0.0.0",
-                port=80
-                )
+# # run code locally
+# import uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app,
+#                 host="0.0.0.0",
+#                 port=80
+#                 )
